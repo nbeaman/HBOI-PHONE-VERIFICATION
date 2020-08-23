015d0f819926f43d3c6819fc8893d6ec40118d2a
nbeaman <nbeaman@fau.edu>
2020-08-23 18:54:14 -0400

    Initial upload

---
+++ TELLMOM.py
@@ -0,0 +1,5 @@
+#===============================================================================
+# for debugging.  This is used to display that something went wrong
+def TELLMOM(subject, what):
+    print("I AM TELLING! This is not a Phone Verification Email." + " SUBJECT: " + subject + " >>" + what + "<<")
+#===============================================================================
